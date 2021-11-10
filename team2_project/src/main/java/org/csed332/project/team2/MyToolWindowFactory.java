0da88a7e79b272d015dca1dd151024bad435ab13
parksbn812 <parksbn812@postech.ac.kr>
2021-11-10 15:33:05 +0900

    Project Setup(2) - add ToolWindow files

---
+++ team2_project/src/main/java/org/csed332/project/team2/MyToolWindowFactory.java
@@ -0,0 +1,23 @@
+package org.csed332.project.team2;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class MyToolWindowFactory implements ToolWindowFactory {
+    /**
+     * Create the tool window content.
+     *
+     * @param project    current project
+     * @param toolWindow current tool window
+     */
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        var projectToolWindow = new ProjectToolWindow(toolWindow);
+        var contentFactory = ContentFactory.SERVICE.getInstance();
+        var content = contentFactory.createContent(projectToolWindow.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
---
+++ team2_project/src/main/java/org/csed332/project/team2/ProjectToolWindow.java
@@ -0,0 +1,16 @@
+package org.csed332.project.team2;
+
+import com.intellij.openapi.wm.ToolWindow;
+
+import javax.swing.*;
+
+public class ProjectToolWindow {
+    private JPanel projectToolWindowContent;
+
+    public ProjectToolWindow(ToolWindow toolWindow) {
+    }
+
+    public JPanel getContent() {
+        return projectToolWindowContent;
+    }
+}
