0b2d1406e54d329d92a3576a84105c585ee4fad6
Minsu Gong <gongms@postech.ac.kr>
2021-11-14 23:50:04 +0900

    test: DB save and get test (#18)

---
+++ team2_project/src/test/java/org/csed332/project/team2/DBTest.java
@@ -0,0 +1,24 @@
+package org.csed332.project.team2;
+
+import org.csed332.project.team2.db.model.MetricModel;
+import org.csed332.project.team2.db.service.MetricModelService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class DBTest {
+    //Make sure that there is no db before running this test!
+    @Test
+    void testDBSaveAndGet() {
+        MetricModel m = new MetricModel();
+        m.setClassName("ClassA");
+        m.setMetric("MetricA");
+        m.setFigure(1.0);
+
+        MetricModelService.save(m);
+
+        MetricModel mNew = MetricModelService.getMetricById(1L);
+        Assertions.assertEquals("ClassA", mNew.getClassName());
+        Assertions.assertEquals("MetricA", mNew.getMetric());
+        Assertions.assertEquals(1.0, mNew.getFigure());
+    }
+}
