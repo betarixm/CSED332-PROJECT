bc321105aef2db47145938d25a9bcbf5970e3472
Minsu Gong <gongms@postech.ac.kr>
2021-11-15 19:19:08 +0900

    test: Empty DB before each test. (minjae, minsu)[#18]

--- team2_project/src/test/java/org/csed332/project/team2/DBTest.java
+++ team2_project/src/test/java/org/csed332/project/team2/DBTest.java
@@ -2,10 +2,22 @@ package org.csed332.project.team2;
 
 import org.csed332.project.team2.db.model.MetricModel;
 import org.csed332.project.team2.db.service.MetricModelService;
+import org.csed332.project.team2.db.util.HibernateUtil;
+import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class DBTest {
+    @BeforeEach
+    void emptyDB() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createQuery("delete from MetricModel ").executeUpdate();
+            session.getTransaction().commit();
+        }
+    }
+
     @Test
     void testDBSaveAndGet() {
         MetricModel m = new MetricModel();
