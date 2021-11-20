75f4d1d24bcb24ff9a2ebec5bb1a8c9a447f1be0
parksbn812 <parksbn812@postech.ac.kr>
2021-11-20 22:35:53 +0900

    feat: add getActiveProject(), getRootPackages() (namgeon, subin) [#2]

---
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/project/ProjectFactory.java
@@ -0,0 +1,62 @@
+package org.csed332.project.team2.metrics.project;
+import com.intellij.ide.projectView.impl.nodes.PackageUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.psi.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class ProjectFactory {
+    /**
+     * Returns the open project of the current IntelliJ IDEA window
+     *
+     * @return the project
+     */
+    @NotNull
+    public static Project getActiveProject() {
+        for (var project : ProjectManager.getInstance().getOpenProjects()) {
+            var window = WindowManager.getInstance().suggestParentWindow(project);
+            if (window != null && window.isActive())
+                return project;
+        }
+        // if there is no active project, return an arbitrary project (the first)
+        return ProjectManager.getInstance().getOpenProjects()[0];
+    }
+
+    /**
+     * Returns the root package(s) in the source directory of a project. The default package will not be considered, as
+     * it includes all Java classes. Note that classes in the default package (i.e., having no package statement) will
+     * be ignored for this assignment. To be completed, this case must be separately handled.
+     *
+     * @param project a project
+     * @return a set of root packages
+     */
+    public static Set<PsiPackage> getRootPackages(Project project) {
+        final Set<PsiPackage> rootPackages = new HashSet<>();
+        var visitor = new PsiElementVisitor() {
+            @Override
+            public void visitDirectory(@NotNull PsiDirectory dir) {
+                final var psiPackage = JavaDirectoryService.getInstance().getPackage(dir);
+                if (psiPackage != null && !PackageUtil.isPackageDefault(psiPackage))
+                    rootPackages.add(psiPackage);
+                else
+                    Arrays.stream(dir.getSubdirectories()).forEach(sd -> sd.accept(this));
+            }
+        };
+
+        var rootManager = ProjectRootManager.getInstance(project);
+        var psiManager = PsiManager.getInstance(project);
+        Arrays.stream(rootManager.getContentSourceRoots())
+                .map(psiManager::findDirectory)
+                .filter(Objects::nonNull)
+                .forEach(dir -> dir.accept(visitor));
+
+        return rootPackages;
+    }
+}
