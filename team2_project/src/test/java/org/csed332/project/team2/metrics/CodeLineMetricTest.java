d24e2b63fe741a93cb22478835491c0fbf56295a
parksbn812 <parksbn812@postech.ac.kr>
2021-11-23 13:54:33 +0900

    test: modify to test with dummy project (namgeon, subin) [#19]

--- team2_project/src/test/java/org/csed332/project/team2/metrics/CodeLineMetricTest.java
+++ team2_project/src/test/java/org/csed332/project/team2/metrics/CodeLineMetricTest.java
@@ -18,23 +18,26 @@ public class CodeLineMetricTest {
     }
 
     @Test
-    void testPackageCalculate(){
-        CodeLineMetric packageCodeLineMetric = new PackageCodeLineMetric(projectCodeLineMetric.getPath() + "/org/csed332/project/team2/metrics");
-        Assertions.assertEquals(packageCodeLineMetric.calculate(), 140.0);
-
-        CodeLineMetric package2CodeLineMetric = new PackageCodeLineMetric(projectCodeLineMetric.getPath() + "/org/csed332/project/team2/");
-        Assertions.assertEquals(package2CodeLineMetric.calculate(), 357.0);
+    public void testPackageCalculate() {
+        // set with your absolute path of your package
+        CodeLineMetric packageCodeLineMetric = new PackageCodeLineMetric(projectCodeLineMetric.getPath() + "/edu/postech/csed332/homework1");
+        Assertions.assertEquals(packageCodeLineMetric.calculate(), 271.0);
     }
 
+    /*
+    test class list:
+    ACCTYPE.java  Bank.java                 IllegalOperationException.java
+    Account.java  HighInterestAccount.java  LowInterestAccount.java
+     */
     @Test
     void testClassCalculate(){
         CodeLineMetric classCodeLineMetric = new ClassCodeLineMetric(projectCodeLineMetric.getPath() + "/org/csed332/project/team2/metrics/ClassCodeLineMetric.java");
         Assertions.assertEquals(classCodeLineMetric.calculate(), 29.0);
 
-        CodeLineMetric classCodeLineMetric2 = new ClassCodeLineMetric(projectCodeLineMetric.getPath() + "/org/csed332/project/team2/metrics/CodeLineMetric.java");
-        Assertions.assertEquals(classCodeLineMetric2.calculate(), 32.0);
+        CodeLineMetric classCodeLineMetric2 = new ClassCodeLineMetric(projectCodeLineMetric.getPath() + "/edu/postech/csed332/homework1/HighInterestAccount.java");
+        Assertions.assertEquals(classCodeLineMetric2.calculate(), 48.0);
 
-        CodeLineMetric classCodeLineMetric3 = new ClassCodeLineMetric(projectCodeLineMetric.getPath() + "/org/csed332/project/team2/metrics/CompositeCodeLineMetric.java");
-        Assertions.assertEquals(classCodeLineMetric3.calculate(), 45.0);
+        CodeLineMetric classCodeLineMetric3 = new ClassCodeLineMetric(projectCodeLineMetric.getPath() + "/edu/postech/csed332/homework1/LowInterestAccount.java");
+        Assertions.assertEquals(classCodeLineMetric3.calculate(), 48.0);
     }
 }
