6ed11e1ecfd19f84b8c73cf833ee2b92f94e35c6
parksbn812 <parksbn812@postech.ac.kr>
2021-11-23 16:01:44 +0900

    feat: implement get() and set() with DB connection (namgeon, subin) [#19]

--- team2_project/src/main/java/org/csed332/project/team2/metrics/ClassCodeLineMetric.java
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/ClassCodeLineMetric.java
@@ -1,13 +1,43 @@
 package org.csed332.project.team2.metrics;
 
+import org.csed332.project.team2.db.model.MetricModel;
+import org.csed332.project.team2.db.service.MetricModelService;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.FileReader;
+import java.util.List;
 
 public class ClassCodeLineMetric extends CodeLineMetric {
 
+    // TODO: class info
+    public String className;
+
     public ClassCodeLineMetric(String path) {
         super(path);
+        // TODO: Set className
+        String namePlusJava = new File(path).getName();
+        this.className = namePlusJava.substring(0, namePlusJava.length() - 5);
+        super.set(-1);
+    }
+
+    // TODO: get() from db
+    //  Using MetricModelService.getMetrics(getID(), [className], 1);
+    //  Case of not in db...
+    @Override
+    public double get() {
+        List<MetricModel> metricModelList = MetricModelService.getMetrics(getID(), className, 1);
+
+        return metricModelList.isEmpty() ? super.get() : metricModelList.get(0).getFigure();
+    }
+
+    // TODO: set(value) to db
+    //  MetricModelService.saveMetrics([getID()], [className], value);
+    @Override
+    protected void set(int value) {
+        super.set(value);
+        MetricModelService.saveMetric(getID(), className, value);
     }
 
     @Override
