eec44f35513ee093ef57533cbb97573af40fb5a1
parksbn812 <parksbn812@postech.ac.kr>
2021-11-23 13:56:06 +0900

    feat: modify to get project path with getBasePath() (namgeon, subin) [#19]

--- team2_project/src/main/java/org/csed332/project/team2/metrics/ProjectCodeLineMetric.java
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/ProjectCodeLineMetric.java
@@ -4,10 +4,30 @@ import java.io.File;
 
 public class ProjectCodeLineMetric extends CompositeCodeLineMetric {
     public ProjectCodeLineMetric(String path) {
-        super(path);
+        super(path + "/src/main/java");
     }
 
     public ProjectCodeLineMetric() {
         this(new File("").getAbsolutePath() + "/src/main/java");
+    }
+
+    public ProjectCodeLineMetric(Project project) {
+        this(project.getBasePath());
+    }
+
+    /**
+     * Returns the open project of the current IntelliJ IDEA window
+     *
+     * @return the project
+     */
+    @NotNull
+    private static Project getActiveProject() {
+        for (var project : ProjectManager.getInstance().getOpenProjects()) {
+            var window = WindowManager.getInstance().suggestParentWindow(project);
+            if (window != null && window.isActive())
+                return project;
+        }
+        // if there is no active project, return an arbitrary project (the first)
+        return ProjectManager.getInstance().getOpenProjects()[0];
     }
 }
