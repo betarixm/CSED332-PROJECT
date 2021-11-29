d561d607523afff25d63926294b1627d96b6e4a9
beta <mzg00@postech.ac.kr>
2021-11-30 00:32:58 +0900

    refactor: Abstract class for Metric (#34)(!22)
    Co-authored-by: eunsue Choi <ches7283@postech.ac.kr>

---
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/BaseMetric.java
@@ -0,0 +1,26 @@
+package org.csed332.project.team2.metrics;
+
+public abstract class BaseMetric {
+    static enum Type {LINES_OF_CODE, CYCLOMATIC}
+
+    private double metric;
+    private String id;
+
+    protected abstract double calculate();
+
+    public double getMetric() {
+        return this.metric;
+    }
+
+    public void setMetric(double metric) {
+        this.metric = metric;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    protected void setId(String id) {
+        this.id = id;
+    }
+}
