bbc7656f8a6c40b24796028dc19d9c5358bf9cb8
beta <mzg00@postech.ac.kr>
2021-11-30 00:33:09 +0900

    refactor: Abstract class for Metric Composite (#34)(!22)
    Co-authored-by: eunsue Choi <ches7283@postech.ac.kr>

---
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/BaseMetricComposite.java
@@ -0,0 +1,27 @@
+package org.csed332.project.team2.metrics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class BaseMetricComposite extends BaseMetric {
+    final private List<BaseMetric> metricList;
+
+    public BaseMetricComposite() {
+        this.metricList = new ArrayList<>();
+    }
+
+    public void addMetric(BaseMetric metricComposite) {
+        this.metricList.add(metricComposite);
+    }
+
+    public List<BaseMetric> getMetricList() {
+        return Collections.unmodifiableList(metricList);
+    }
+
+    @Override
+    public double calculate() {
+        setMetric(metricList.stream().mapToDouble(BaseMetric::calculate).sum());
+        return getMetric();
+    }
+}
