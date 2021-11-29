6755b82861e7201c411a3938e965c942cabcaba7
beta <mzg00@postech.ac.kr>
2021-11-30 00:34:15 +0900

    feat: Root class for cyclomatic metric (#29)(!21)
    Co-authored-by: eunsue Choi <ches7283@postech.ac.kr>

---
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/cyclomatic/CyclomaticMetric.java
@@ -0,0 +1,9 @@
+package org.csed332.project.team2.metrics.cyclomatic;
+
+import org.csed332.project.team2.metrics.BaseMetricComposite;
+
+public class CyclomaticMetric extends BaseMetricComposite {
+    public CyclomaticMetric() {
+        setId("cyclomatic");
+    }
+}
