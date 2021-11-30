b30cfd7d68cfb63274061a19703cdcc64e8651c3
beta <mzg00@postech.ac.kr>
2021-11-30 20:46:41 +0900

    feat: Impl. visitor for calculating cyclomatic metric (#29)
    Co-authored-by: eunsue Choi <ches7283@postech.ac.kr>

--- team2_project/src/main/java/org/csed332/project/team2/metrics/cyclomatic/CyclomaticMetric.java
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/cyclomatic/CyclomaticMetric.java
@@ -3,6 +3,8 @@ package org.csed332.project.team2.metrics.cyclomatic;
 import com.intellij.psi.*;
 import org.csed332.project.team2.metrics.VisitingMetric;
 
+import java.util.Objects;
+
 public class CyclomaticMetric extends VisitingMetric {
     public CyclomaticMetric(PsiElement element) {
         super(element);
@@ -11,21 +13,52 @@ public class CyclomaticMetric extends VisitingMetric {
 
     @Override
     protected void visitPackageMetric(PsiPackage pack) {
-
+        for (PsiClass aClass : pack.getClasses()) {
+            aClass.accept(visitor);
+        }
     }
 
     @Override
     protected void visitClassMetric(PsiClass aClass) {
-
+        for (PsiMethod method : aClass.getMethods()) {
+            double value = getVisitResult();
+            method.accept(visitor);
+            setMetric(getVisitResult() - value, aClass, method);
+        }
     }
 
     @Override
     protected void visitMethodMetric(PsiMethod method) {
+        PsiCodeBlock codes = method.getBody();
+
+        if (codes != null) {
+            for (PsiStatement statement : codes.getStatements()) {
+                statement.accept(visitor);
+            }
+        }
 
     }
 
     @Override
     protected void visitFieldMetric(PsiField field) {
 
+    }
+
+    @Override
+    protected void visitBranchMetric(PsiConditionalExpression expr) {
+        setVisitResult(getVisitResult() + 1);
+
+        expr.getCondition().accept(visitor);
+        Objects.requireNonNull(expr.getThenExpression()).accept(visitor);
+        Objects.requireNonNull(expr.getElseExpression()).accept(visitor);
+    }
+
+    @Override
+    protected void visitSwitchMetric(PsiSwitchLabelStatement sLabel) {
+        if (!sLabel.isDefaultCase()) {
+            setVisitResult(getVisitResult() + 1);
+        }
+
+        Objects.requireNonNull(sLabel.getEnclosingSwitchBlock()).accept(visitor);
     }
 }
