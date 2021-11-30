ef59d1e56c0b6cb9ffa109edfe4672f6627b4953
beta <mzg00@postech.ac.kr>
2021-11-30 12:09:26 +0900

    feat: Prototyping cyclomatic metric with MetricVisitor (#29)(#34)(!21)(!22)
    Co-authored-by: eunsue Choi <ches7283@postech.ac.kr>

--- team2_project/src/main/java/org/csed332/project/team2/metrics/cyclomatic/CyclomaticMetric.java
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/cyclomatic/CyclomaticMetric.java
@@ -1,9 +1,31 @@
 package org.csed332.project.team2.metrics.cyclomatic;
 
-import org.csed332.project.team2.metrics.BaseMetricComposite;
+import com.intellij.psi.*;
+import org.csed332.project.team2.metrics.MetricVisitor;
 
-public class CyclomaticMetric extends BaseMetricComposite {
-    public CyclomaticMetric() {
+public class CyclomaticMetric extends MetricVisitor {
+    public CyclomaticMetric(PsiElement element) {
+        super(element);
         setId("cyclomatic");
+    }
+
+    @Override
+    protected void visitPackageMetric(PsiPackage pack) {
+
+    }
+
+    @Override
+    protected void visitClassMetric(PsiClass aClass) {
+
+    }
+
+    @Override
+    protected void visitMethodMetric(PsiMethod method) {
+
+    }
+
+    @Override
+    protected void visitFieldMetric(PsiField field) {
+
     }
 }
