05e9e3fe06d27ab4e4800e66682ababdd7beaa64
beta <mzg00@postech.ac.kr>
2021-11-30 12:36:08 +0900

    feat: Visitor with directory traversal (#34)(!22)
    Co-authored-by: eunsue Choi <ches7283@postech.ac.kr>

--- team2_project/src/main/java/org/csed332/project/team2/metrics/MetricVisitor.java
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/MetricVisitor.java
@@ -1,10 +1,50 @@
 package org.csed332.project.team2.metrics;
 
+import com.intellij.ide.projectView.impl.nodes.PackageUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.psi.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public abstract class MetricVisitor extends BaseMetric {
     private double visitResult = 0;
     private PsiElement psiElement;
+    private Project project;
+
+    final PsiElementVisitor visitor = new JavaElementVisitor() {
+        @Override
+        public void visitDirectory(@NotNull PsiDirectory dir) {
+            final var psiPackage = JavaDirectoryService.getInstance().getPackage(dir);
+            if (psiPackage != null && !PackageUtil.isPackageDefault(psiPackage)) {
+                psiPackage.accept(this);
+            } else {
+                Arrays.stream(dir.getSubdirectories()).forEach(sd -> sd.accept(this));
+            }
+        }
+
+        @Override
+        public void visitPackage(PsiPackage pack) {
+            visitPackageMetric(pack);
+        }
+
+        @Override
+        public void visitClass(PsiClass aClass) {
+            visitClassMetric(aClass);
+        }
+
+        @Override
+        public void visitMethod(PsiMethod method) {
+            visitMethodMetric(method);
+        }
+
+        @Override
+        public void visitField(PsiField field) {
+            visitFieldMetric(field);
+        }
+    };
 
     public MetricVisitor(PsiElement element) {
         setPsiElement(element);
@@ -35,36 +75,30 @@ public abstract class MetricVisitor extends BaseMetric {
     }
 
     protected double visit(PsiElement psiElement) {
-        final var visitor = new JavaElementVisitor() {
-            @Override
-            public void visitPackage(PsiPackage pack) {
-                visitPackageMetric(pack);
-            }
-
-            @Override
-            public void visitClass(PsiClass aClass) {
-                visitClassMetric(aClass);
-            }
-
-            @Override
-            public void visitMethod(PsiMethod method) {
-                visitMethodMetric(method);
-            }
-
-            @Override
-            public void visitField(PsiField field) {
-                visitFieldMetric(field);
-            }
-        };
-
         psiElement.accept(visitor);
+        return getVisitResult();
+    }
+
+    protected double visit(Project project) {
+        var rootManager = ProjectRootManager.getInstance(project);
+        var psiManager = PsiManager.getInstance(project);
+
+        Arrays.stream(rootManager.getContentSourceRoots())
+                .map(psiManager::findDirectory)
+                .filter(Objects::nonNull)
+                .forEach(dir -> dir.accept(visitor));
 
         return getVisitResult();
     }
 
     @Override
     public double calculate() {
-        setMetric(visit(this.psiElement));
+        if (psiElement != null) {
+            setMetric(visit(psiElement));
+        } else if (project != null) {
+            setMetric(visit(project));
+        }
+
         return getMetric();
     }
 }
