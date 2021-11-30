7f9808d5b6ddb34db46e6612afbdbd3ec379a486
beta <mzg00@postech.ac.kr>
2021-11-30 20:45:49 +0900

    feat: Support for managing hierarchy metrics (#34)
    Co-authored-by: eunsue Choi <ches7283@postech.ac.kr>

--- team2_project/src/main/java/org/csed332/project/team2/metrics/BaseMetric.java
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/BaseMetric.java
@@ -1,10 +1,23 @@
 package org.csed332.project.team2.metrics;
 
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiMethod;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class BaseMetric {
     static enum Type {LINES_OF_CODE, CYCLOMATIC}
 
     private double metric;
     private String id;
+
+    private final Map<PsiClass, Map<PsiMethod, Double>> metrics;
+
+    public BaseMetric() {
+        metrics = new HashMap<>();
+    }
 
     public abstract double calculate();
 
@@ -12,8 +25,28 @@ public abstract class BaseMetric {
         return this.metric;
     }
 
+    public Double getMetric(PsiClass psiClass, PsiMethod psiMethod) {
+        if (metrics.containsKey(psiClass)) {
+            return metrics.get(psiClass).get(psiMethod);
+        }
+
+        return null;
+    }
+
+    public Map<PsiClass, Map<PsiMethod, Double>> getMetrics() {
+        return Collections.unmodifiableMap(metrics);
+    }
+
     public void setMetric(double metric) {
         this.metric = metric;
+    }
+
+    public void setMetric(double metric, PsiClass psiClass, PsiMethod psiMethod) {
+        if (!metrics.containsKey(psiClass)) {
+            metrics.put(psiClass, new HashMap<>());
+        }
+
+        metrics.get(psiClass).put(psiMethod, metric);
     }
 
     public String getId() {
