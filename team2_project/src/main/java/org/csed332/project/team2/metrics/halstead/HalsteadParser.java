6495b893b1e4e7fab0a5cd5d906a337fc666f787
parksbn812 <parksbn812@postech.ac.kr>
2021-12-03 19:11:11 +0900

    feat: implement halstead parser (namgeon, subin) [#28]

--- team2_project/src/main/java/org/csed332/project/team2/metrics/halstead/HalsteadParser.java
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/halstead/HalsteadParser.java
@@ -1,12 +1,32 @@
 package org.csed332.project.team2.metrics.halstead;
 
 import com.intellij.psi.PsiMethod;
+import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 
 // TODO: parse method... with ASTParser
 //  then visit all nodes to count the number of total operators
 public class HalsteadParser {
-    public static void parse(PsiMethod method){
+    private HalsteadVisitor halsteadVisitor;
 
+    public HalsteadParser() {
+        this.halsteadVisitor = new HalsteadVisitor();
+    }
+
+    public void parse(PsiMethod method) {
+        ASTParser parser = ASTParser.newParser(AST.JLS3);
+        String fakeClassText = "public class A {" + method.getText() + "}";
+        parser.setSource(fakeClassText.toCharArray());
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+
+        final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+        cu.accept(this.halsteadVisitor);
+    }
+
+
+    public HalsteadVisitor getHalsteadVisitor() {
+        return halsteadVisitor;
     }
 }
---
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/halstead/HalsteadVisitor.java
@@ -0,0 +1,96 @@
+package org.csed332.project.team2.metrics.halstead;
+
+import org.eclipse.jdt.core.dom.*;
+
+import java.util.HashMap;
+
+public class HalsteadVisitor extends ASTVisitor {
+    public HashMap<String, Integer> operands = new HashMap<>();
+    public HashMap<String, Integer> operators = new HashMap<>();
+
+    public boolean visit(CompilationUnit unit) {
+        return true;
+    }
+
+    public boolean visit(ForStatement node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public boolean visit(InfixExpression node) {
+        addOperator(node.getOperator().toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(PrefixExpression node) {
+        addOperator(node.getOperator().toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(PostfixExpression node) {
+        addOperator(node.getOperator().toString());
+        return true;
+    }
+
+    @Override
+    public boolean visit(Assignment node) {
+        addOperator(node.getOperator().toString());
+        return true;
+    }
+
+    public boolean visit(VariableDeclaration node) {
+        addOperator("=");
+        return true;
+    }
+
+    @Override
+    public boolean visit(SimpleName name) {
+        addOperand(name.getIdentifier());
+        return true;
+    }
+
+    @Override
+    public boolean visit(NullLiteral literal) {
+        addOperand("null");
+        return true;
+    }
+
+    @Override
+    public boolean visit(StringLiteral literal) {
+        addOperand(literal.getLiteralValue());
+        return true;
+    }
+
+    @Override
+    public boolean visit(CharacterLiteral literal) {
+        addOperand(Character.toString(literal.charValue()));
+        return true;
+    }
+
+    @Override
+    public boolean visit(BooleanLiteral literal) {
+        addOperand(Boolean.toString(literal.booleanValue()));
+        return true;
+    }
+
+    @Override
+    public boolean visit(NumberLiteral literal) {
+        addOperand(literal.getToken());
+        return true;
+    }
+
+
+    public void addOperator(String operator) {
+        if (!operators.containsKey(operator))
+            operators.put(operator, 0);
+        operators.put(operator, operators.get(operator) + 1);
+    }
+
+    public void addOperand(String operand) {
+        if (!operands.containsKey(operand))
+            operands.put(operand, 0);
+        operands.put(operand, operands.get(operand) + 1);
+    }
+}
