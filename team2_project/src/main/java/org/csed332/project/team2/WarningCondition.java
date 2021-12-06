bf9748e6caafb924990ff9ff965273b14473d605
Noémie Jacquemot <njacquemot@postech.ac.kr>
2021-12-06 09:30:44 +0900

    feat: Coded basic functions and API in WarningCondition (#30, !23)
    Co-authored-by: Minsu Gong <gongms@postech.ac.kr>

--- team2_project/src/main/java/org/csed332/project/team2/WarningCondition.java
+++ team2_project/src/main/java/org/csed332/project/team2/WarningCondition.java
@@ -1,4 +1,35 @@
 package org.csed332.project.team2;
 
+import java.util.Optional;
+
 public class WarningCondition {
+
+    public enum Mode {INCREASE,DECREASE,MORE_THAN, LESS_THAN}
+
+    private double threshold;
+    private double teta;
+    private Mode mode;
+
+    public WarningCondition(Mode mode){
+        setMode(mode);
+        teta = 0;
+        threshold = 0;
+    }
+    public WarningCondition(Mode mode, double threshold){
+        this(mode);
+        setThreshold(threshold);
+    }
+
+    public void setMode(Mode mode){
+        this.mode = mode;
+    }
+    public void setThreshold(double value){
+        threshold = value;
+    }
+    public void setTeta(double value){
+        teta = value;
+    }
+    public Boolean shouldWarn(double value){
+        return true;
+    }
 }
