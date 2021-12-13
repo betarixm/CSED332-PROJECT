3e5282fc5c96f27eec440d9b915d4755d2af8c04
Nam Geon Kim <namgeon1106@postech.ac.kr>
2021-12-13 20:49:13 +0900

    feat: implement getDegradationMetrics() on BaseMetric(noemie, namgeon)[#44]

--- team2_project/src/main/java/org/csed332/project/team2/metrics/BaseMetric.java
+++ team2_project/src/main/java/org/csed332/project/team2/metrics/BaseMetric.java
@@ -1,19 +1,18 @@
 package org.csed332.project.team2.metrics;
 
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiMethod;
 
+import org.apache.tools.ant.taskdefs.War;
 import org.csed332.project.team2.db.model.CalcHistoryModel;
 import org.csed332.project.team2.db.service.MetricModelService;
 import org.csed332.project.team2.db.service.MetricService;
 import org.csed332.project.team2.WarningCondition;
 import org.csed332.project.team2.db.model.MetricModel;
-import org.csed332.project.team2.db.service.MetricModelService;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public abstract class BaseMetric implements Metric {
     protected enum Type {LINES_OF_CODE, CYCLOMATIC, HALSTEAD}
@@ -23,6 +22,10 @@ public abstract class BaseMetric implements Metric {
     protected WarningCondition cond;
 
     private final Map<PsiClass, Map<PsiMethod, Double>> metrics;
+
+    public void setCondition(WarningCondition cond){
+        this.cond = cond;
+    }
 
     public BaseMetric() {
         metrics = new HashMap<>();
@@ -70,9 +73,42 @@ public abstract class BaseMetric implements Metric {
         this.id = id;
     }
 
+    public Map<PsiClass, Set<PsiMethod>> getDegradationMetrics(){
+        Map<PsiClass, Set<PsiMethod>> degradedMetrics = new HashMap<>();
+
+        List<CalcHistoryModel> query = MetricService.query(getID(), 2);
+        query.stream().filter(calcHistoryModel -> {
+            List<MetricModel> metricModels = new ArrayList<>(calcHistoryModel.getMetricModels());
+            if(metricModels.size() < 2) return false;
+
+            Double olderValue = metricModels.get(0).getFigure();
+            Double newValue = metricModels.get(1).getFigure();
+
+            return cond.shouldWarn(olderValue, newValue);
+        }).forEach(calcHistoryModel -> {
+            MetricModel metricModel = calcHistoryModel.getMetricModels().iterator().next();
+            String className = metricModel.getClassName();
+            String methodName = metricModel.getMethodName();
+
+            PsiClass psiClass = metrics.keySet().stream()
+                    .filter(aClass -> aClass.getName().equals(className))
+                    .findFirst().get();
+
+            PsiMethod psiMethod = metrics.get(psiClass).keySet().stream()
+                    .filter(aMethod -> aMethod.getName().equals(methodName))
+                    .findFirst().get();
+
+            degradedMetrics.putIfAbsent(psiClass, new HashSet<>());
+            degradedMetrics.get(psiClass).add(psiMethod);
+        });
+
+        return Collections.unmodifiableMap(degradedMetrics);
+    }
+
     @Override
     public boolean checkDegradation() {
         //TODO: make this method return true if value of Halstead Metric degraded.
+        return false;
     }
 
     // save metrics to DB
