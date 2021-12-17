605e9e805e2a92b77c01b1b2be43932c14121704
parksbn812 <parksbn812@postech.ac.kr>
2021-12-17 11:37:30 +0900

    test: add maintainability metric test (subin, minsu) [#60]

---
+++ team2_project/src/test/java/org/csed332/project/team2/metrics/maintainability/MaintainabilityMetricTest.java
@@ -0,0 +1,76 @@
+package org.csed332.project.team2.metrics.maintainability;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiMethod;
+import org.csed332.project.team2.FixtureHelper;
+import org.csed332.project.team2.db.model.CalcHistoryModel;
+import org.csed332.project.team2.db.util.HibernateUtil;
+import org.hibernate.Session;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaintainabilityMetricTest {
+    private static final String testPath = "TestProjects/SingleFiles";
+    private static FixtureHelper helperMainClass;
+
+    @BeforeAll
+    public static void initialize() throws Exception {
+        helperMainClass = FixtureHelper.getInstance();
+        helperMainClass.changeFile(testPath);
+        helperMainClass.setUp();
+    }
+
+    @AfterAll
+    public static void dispose() throws Exception {
+        helperMainClass.tearDown();
+    }
+
+    private void configureFixture(String fileName) throws Exception {
+        helperMainClass.configure(fileName);
+    }
+
+    @Test
+    public void testMaintainability() throws Exception {
+        configureFixture("MultiMethods.java");
+        ApplicationManager.getApplication()
+                .invokeAndWait(
+                        () -> {
+                            final Project project = helperMainClass.getFixture().getProject();
+                            final PsiClass psiClass = helperMainClass.getFirstPsiClass();
+
+                            MaintainabilityMetric maintainabilityMetric = new MaintainabilityMetric(psiClass);
+                            maintainabilityMetric.calculate();
+
+                            Map<PsiMethod, Double> metrics = maintainabilityMetric.getMetrics().get(psiClass.getName());
+                            Map<String, PsiMethod> methods = getMethods(psiClass);
+
+                            Assertions.assertEquals(1.0, metrics.get(methods.get("simpleAddition")));
+                        });
+    }
+
+    void cleanDB(CalcHistoryModel calc) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.remove(calc);
+            session.getTransaction().commit();
+        }
+    }
+
+    public Map<String, PsiMethod> getMethods(PsiClass aClass) {
+        Map<String, PsiMethod> result = new HashMap<>();
+
+        for (PsiMethod method : aClass.getMethods()) {
+            result.put(method.getName(), method);
+        }
+
+        return result;
+    }
+
+}
